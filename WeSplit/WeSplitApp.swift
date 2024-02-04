75fa7b482495b07aaa03da793091fc84027cc77a
Lucas Pennice <lucaspennice@gmail.com>
2024-02-04 20:59:50 -0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Lucas Pennice on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
